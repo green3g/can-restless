9263332a3a09b3103d9e8e3cfe6cbef60a146884
roemhildtg <roemhildtg@gmail.com>
2016-07-09 12:59:35 -0500

    Tests

--- test/main.js
+++ test/main.js
@@ -1,8 +1,2 @@
-
-import 'filter-widget/filter-widget.test';
-import 'list-table/list-table.test';
-import 'property-table/property-table.test';
-import 'can-restless-manager/can-restless-manager.test';
-import 'util/string.test';
-import 'paginate-widget/paginate-widget.test';
-import 'form-widget/form-widget.test';
+import '../lib/CanRestless.test';
+import '../lib/Params.test';
